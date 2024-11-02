7f0a6b3486a5e9e615c26f0589191627dc497568
Sara01-s <saraosses01@gmail.com>
2024-11-02 18:24:07 -0300

    backup (ui): timeline ui implementation started

--- Assets/Scripts/Views/VideoTimelineView.cs
+++ Assets/Scripts/Views/VideoTimelineView.cs
@@ -7,13 +7,21 @@ namespace SimpleMotions {
 
 		[SerializeField] private Button _createTestEntity;
 		[SerializeField] private Slider _cursor;
+		[SerializeField] private RectTransform _contentArea;
+		[SerializeField] private Sprite _whiteTexture;
 
         public void Configure(IVideoTimelineViewModel videoTimelineViewModel) {
 			_createTestEntity.onClick.AddListener(() => videoTimelineViewModel.OnCreateTestEntity.Execute(null));
 			_cursor.onValueChanged.AddListener(value => videoTimelineViewModel.OnSetCurrentFrame.Execute((int)value));
 
 			videoTimelineViewModel.OnTimelineUpdate.Subscribe(SetCursorValue);
+
+			GenerateTimeline();
         }
+
+		private void GenerateTimeline() {
+			
+		}
 
 		private void SetCursorValue(VideoDisplayInfo videoDisplayInfo) {
 			_cursor.value = videoDisplayInfo.CurrentFrame;
